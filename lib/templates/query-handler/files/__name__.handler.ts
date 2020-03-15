9750b4bc5b58969a6e52431a516e11ae4bb1ef46
Juanjo Chust <nytyr@me.com>
2020-03-15 20:14:22 +0100

    🐛 Fix spaces

--- lib/templates/query-handler/files/__name__.handler.ts
+++ lib/templates/query-handler/files/__name__.handler.ts
@@ -4,6 +4,6 @@ import { %className%Query } from '../impl/%name%.query';
 @QueryHandler(%className%Query)
 export class %className%Handler implements IQueryHandler<%className%Query> {
 
-  async execute(query: %className%Query) {
-  }
+    async execute(query: %className%Query) {
+    }
 }
